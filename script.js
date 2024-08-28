0a9f3aaec90e01d1d5b08cdf21bbc43ef0d91a57
Can Ngo <44318634+nncan99@users.noreply.github.com>
2024-08-28 15:40:13 +0700

    feat: function number click

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 const screen = document.querySelector('#screen');
+const numBtns = document.querySelectorAll('.number');
 screen.textContent = 'abc'
+
+numBtns.forEach( btn => {
+    console.log(btn.value)
+})
