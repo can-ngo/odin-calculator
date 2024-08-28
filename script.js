f0ac02b454b2a54a2ed9eaf431ee716c4d2e69df
Can Ngo <44318634+nncan99@users.noreply.github.com>
2024-08-28 16:23:47 +0700

    add: number one and two and display

--- script.js
+++ script.js
@@ -8,34 +8,49 @@ const clearBtn = document.querySelector('#clear-btn');
 const equalBtn = document.querySelector('#equal');
 const decimalBtn = document.querySelector('#decimal-btn');
 
-let numberOne = { value: undefined };
-let numberTwo = { value: undefined };
-let display = { value: undefined };
+let numberOne = { value: '' };
+let numberTwo = { value: '' };
+let operator = { value: '' };
+let display = { value: '' };
 
-screen.textContent = display.value;
 
 numBtns.forEach( btn => {
-    
+    btn.addEventListener('click', (event) => {
+        if (!operator.value){
+            numberOne.value += event.target.value;
+            display.value = numberOne.value;
+            screen.textContent = display.value;
+            console.log('num 1: ', numberOne.value)
+        } else {
+            numberTwo.value += event.target.value;
+            screen.textContent = display.value;
+            console.log('num 2: ', numberTwo.value)
+        }
+    })
 })
 
 addBtn.addEventListener('click', () => {
-    console.log("hello")
+    operator.value = '+';
 })
 subtractBtn.addEventListener('click', () => {
-    console.log("hello")
+    operator.value = '-';
 })
 multiplyBtn.addEventListener('click', () => {
-    console.log("hello")
+    operator.value = 'x';
 })
 divideBtn.addEventListener('click', () => {
-    console.log("hello")
-})
-clearBtn.addEventListener('click', () => {
-    console.log("hello")
+    operator.value = '/';
 })
 equalBtn.addEventListener('click', () => {
     console.log("hello")
 })
 decimalBtn.addEventListener('click', () => {
     console.log("hello")
+})
+clearBtn.addEventListener('click', () => {
+    numberOne.value = '';
+    numberTwo.value = '';
+    operator.value = '';
+    display.value = '';
+    screen.textContent = display.value;
 })
