41fa54f7b13893e07015df5fb9b9771c24f4cb49
Can Ngo <44318634+nncan99@users.noreply.github.com>
2024-08-28 15:58:28 +0700

    test: click event test

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
 const screen = document.querySelector('#screen');
 const numBtns = document.querySelectorAll('.number');
-screen.textContent = 'abc'
+const addBtn = document.querySelector('#add');
+const subtractBtn = document.querySelector('#subtract');
+const multiplyBtn = document.querySelector('#multiply');
+const divideBtn = document.querySelector('#divide');
+const clearBtn = document.querySelector('#clear-btn');
+const equalBtn = document.querySelector('#equal');
+const decimalBtn = document.querySelector('#decimal-btn');
+
+let numberOne = { value: undefined };
+let numberTwo = { value: undefined };
+let display = { value: undefined };
+
+screen.textContent = display.value;
 
 numBtns.forEach( btn => {
-    console.log(btn.value)
+    
+})
+
+addBtn.addEventListener('click', () => {
+    console.log("hello")
+})
+subtractBtn.addEventListener('click', () => {
+    console.log("hello")
+})
+multiplyBtn.addEventListener('click', () => {
+    console.log("hello")
+})
+divideBtn.addEventListener('click', () => {
+    console.log("hello")
+})
+clearBtn.addEventListener('click', () => {
+    console.log("hello")
+})
+equalBtn.addEventListener('click', () => {
+    console.log("hello")
+})
+decimalBtn.addEventListener('click', () => {
+    console.log("hello")
 })
