36d64acae8ece8bb48f37f420e89d9afaf587eb5
Can Ngo <44318634+nncan99@users.noreply.github.com>
2024-08-29 10:10:51 +0700

    fix: operators and add decimal

--- script.js
+++ script.js
@@ -4,40 +4,53 @@ const operatorBtns = document.querySelectorAll('.operator');
 const clearBtn = document.querySelector('#clear-btn');
 const equalBtn = document.querySelector('#equal');
 const decimalBtn = document.querySelector('#decimal-btn');
+const testDiv = document.querySelector('#test');
 
 let numberOne = { value: '' };
 let numberTwo = { value: '' };
 let operator = { value: '' };
 let display = { value: '' };
 
-
 numBtns.forEach( btn => {
     btn.addEventListener('click', (event) => {
+        
         if (!operator.value){
             numberOne.value += event.target.value;
             display.value = numberOne.value;
-            screen.textContent = display.value;
-            console.log('num 1: ', numberOne.value)
+            screen.textContent = display.value; 
         } else {
             numberTwo.value += event.target.value;
             display.value = numberTwo.value;
             screen.textContent = display.value;
-            console.log('num 2: ', numberTwo.value)
         }
+    // testDiv.textContent=`${numberOne.value}   ${operator.value}   
+    //                      ${numberTwo.value} = [ ${display.value} ]`
     })
 })
 
 operatorBtns.forEach( btn => {
-    btn.addEventListener('click', (event) => {
-        if (!operator.value || !numberTwo.value) {
+    btn.addEventListener('click', (event) => { 
+        if ( !numberOne.value && !numberTwo.value)
+        {
+            if (event.target.value === '-'){
+                numberOne.value = '-'
+            } else {
+                operator.value = '';
+            }
+        } else if ( numberOne.value && !numberTwo.value) 
+        {
             operator.value = event.target.value;
+
         } else {
             display.value = operate(numberOne.value, operator.value, numberTwo.value);
             screen.textContent = display.value;
             numberOne.value = display.value.toString();
             numberTwo.value = '';
+            operator.value = event.target.value;
         }
-        console.log(event.target.value)
+        
+    // testDiv.textContent=`${numberOne.value}   ${operator.value}   
+    //                      ${numberTwo.value} = [ ${display.value} ]`
     })
 })
 
@@ -47,11 +60,11 @@ equalBtn.addEventListener('click', () => {
         operator.value = '';
     } else {
         display.value = operate(numberOne.value, operator.value, numberTwo.value);
-        if (display.value === Infinity || display.value === -Infinity) {
+        if (display.value === 'Infinity' || display.value === '-Infinity') {
             numberOne.value = '';
             numberTwo.value = '';
             operator.value = '';
-            display.value = "Error";
+            display.value = "Error: Can't divide by 0";
             screen.textContent = display.value;
             return
         } else {
@@ -61,10 +74,18 @@ equalBtn.addEventListener('click', () => {
             operator.value = '';
         }
     }
+    // testDiv.textContent=`${numberOne.value}   ${operator.value}   
+    //                      ${numberTwo.value} = [ ${display.value} ]`
 })
 
 decimalBtn.addEventListener('click', () => {
-    console.log("hello")
+    if ( !operator.value && !numberOne.value.includes('.')){
+        numberOne.value += '.'
+    } else if ( operator.value && !numberTwo.value.includes('.')) {
+        numberTwo.value += '.'
+    }
+    // testDiv.textContent=`${numberOne.value}   ${operator.value}   
+    //                      ${numberTwo.value} = [ ${display.value} ]`
 })
 
 clearBtn.addEventListener('click', () => {
@@ -73,6 +94,9 @@ clearBtn.addEventListener('click', () => {
     operator.value = '';
     display.value = '';
     screen.textContent = display.value;
+
+    // testDiv.textContent=`${numberOne.value}   ${operator.value}   
+    //                      ${numberTwo.value} = [ ${display.value} ]`
 })
 
 function operate(firstNum, operatorInput, secondNum){
@@ -86,16 +110,16 @@ function operate(firstNum, operatorInput, secondNum){
         switch (operatorInput){
             case '+':
                 operatorInput.value = '';
-                return firstNum + secondNum;
+                return (firstNum + secondNum).toString();
             case '-':
                 operatorInput.value = '';
-                return firstNum - secondNum;
+                return (firstNum - secondNum).toString();
             case 'x':
                 operatorInput.value = '';
-                return firstNum * secondNum;
+                return (firstNum * secondNum).toString();
             case '/':
                 operatorInput.value = '';
-                return firstNum / secondNum;
+                return (firstNum / secondNum).toString();
         }
     
 }
